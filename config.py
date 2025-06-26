de798a4d656f509c1d8581c7f0e5d9a8cb0ffc0b
Oluwaferanmi <feranmi1jaiyeoba@gmail.com>
2025-06-26 12:54:15 +0100

    Update .gitignore and config settings: include instance and database files, adjust SQLAlchemy URI

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Define the configuration class
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///" + os.path.join(basedir, "app.db"))
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Email server settings
